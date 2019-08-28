033e40190c8d7ea11b9a66da1ee5bec453f73110
Juan Paolo B. Nepomuceno (Indra Philippines <v-jpbnepomuceno@meralco.com.ph>
2019-08-28 14:25:27 +0800

    add tabs for mapcache testing.

--- app/src/main/java/com/example/in441635/tomsampleproject/MainActivity.java
+++ app/src/main/java/com/example/in441635/tomsampleproject/MainActivity.java
@@ -1,52 +1,110 @@
 package com.example.in441635.tomsampleproject;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.in441635.tomsampleproject.fragments.SampleFragment1;
+import com.example.in441635.tomsampleproject.fragments.MapCacheFragment;
+import com.example.in441635.tomsampleproject.fragments.SampleFragment3;
+
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+
+    private ActionBar actionBar;
+
+    private FragmentManager fragmentManager;
+
+    private SampleFragment1 sampleFragment1;
+
+    private MapCacheFragment mapCacheFragment;
+
+    private SampleFragment3 sampleFragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        actionBar = getActionBar();
+        fragmentManager = getSupportFragmentManager();
+
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab tab1 = actionBar.newTab().setText("TAB 1").setTabListener(this);
+        ActionBar.Tab tab2 = actionBar.newTab().setText("Mapcache Tab").setTabListener(this);
+        ActionBar.Tab tab3 = actionBar.newTab().setText("TAB 3").setTabListener(this);
+
+        actionBar.addTab(tab1);
+        actionBar.addTab(tab2);
+        actionBar.addTab(tab3);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, sampleFragment1, null).commit();
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Fragment frag = null;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (sampleFragment1 == null) {
+
+            sampleFragment1 = new SampleFragment1();
+
         }
 
-        return super.onOptionsItemSelected(item);
+        if (mapCacheFragment == null) {
+
+            mapCacheFragment = new MapCacheFragment();
+
+        }
+
+        if (sampleFragment3 == null) {
+
+            sampleFragment3 = new SampleFragment3();
+
+        }
+
+        switch (tab.getPosition()){
+
+            case 0:{
+
+                frag = sampleFragment1;
+                break;
+
+            }
+
+            case 1:{
+
+                frag = mapCacheFragment;
+                break;
+
+            }
+
+            case 2:{
+
+                frag = sampleFragment3;
+                break;
+
+            }
+
+            default:
+        }
+
+        fragmentManager.beginTransaction().replace(R.id.container, frag, null).commit();
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 }
